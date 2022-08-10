a1bd8c6ada560e29ce199c17bbf2b52596cb0325
ASWATHI23 <110898037+ASWATHI23@users.noreply.github.com>
2022-08-10 13:51:11 +0530

    third commit

---
+++ list4.py
@@ -0,0 +1,10 @@
+remove_list=[1,2,3,4,5,"Hello","Job",True,False,None,[1,2,3,4,5,6,7,8,9,10]]
+print(remove_list)
+remove_list.remove(1)
+print(remove_list)
+remove_list.remove(2)
+print(remove_list)
+remove_list.pop(0)
+print(remove_list)
+del remove_list[0]
+print(remove_list)
