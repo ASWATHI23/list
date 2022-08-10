e3d8d1d0315317178a5328c2576e8c05ccccb7a2
ASWATHI23 <110898037+ASWATHI23@users.noreply.github.com>
2022-08-10 13:54:46 +0530

    sixth commit

---
+++ list7.py
@@ -0,0 +1,12 @@
+newlist=[1,2,3,4,5,6]
+print(newlist)
+newlist.clear()
+print(newlist)
+
+thislist=["Apple","Banana","Cherry","grape"]
+print(thislist)
+print(thislist.sort())
+print(thislist.sort(reverse=True))
+thislist.sort(reverse=True)
+print(thislist)
+
